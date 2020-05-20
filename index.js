ebba9ee132f9b45653a25d534cb0616592c135a5
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2020-05-20 22:43:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,9 @@ client.on('ready', () => {
 })
  
 client.on('message', msg => {
-  console.log(`msg.content:`)
-  console.log(msg.content)
   if (msg.content.includes('!lotus')) {
-    if (!msg.content.startsWith("!") || msg.author.bot) return;
+    if (!msg.content.startsWith("!") || msg.author.bot) {return}
     const command = msg.content.toLowerCase().replace('!lotus', '').split(' ')[1]
-    console.log(`command:`)
-    console.log(command)
     const regex = new RegExp('^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$')
 
     if (typeof command === 'undefined') {
@@ -36,6 +32,10 @@ client.on('message', msg => {
       const next = dayjs(nowobj).add(45, 'minute').format('HH:mm')
     
       msg.channel.send(`TAKEN: ${now}\nNEXT: ${next}`)
+    }
+   
+    if (typeof command === 'string') {
+      msg.reply(`Invalid input`)
     }
   }
 })
