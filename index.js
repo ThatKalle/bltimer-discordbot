631aa4329f07fb32090dd98101843d582a67a3cd
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2020-05-20 19:42:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,70 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
+const Discord = require('discord.js')
+const client = new Discord.Client()
+ 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-function dateAdd(date, interval, units) {
-  if(!(date instanceof Date))
-    return undefined;
-  var ret = new Date(date); //don't change original date
-  var checkRollover = function() { if(ret.getDate() != date.getDate()) ret.setDate(0);};
-  switch(String(interval).toLowerCase()) {
-    case 'year'   :  ret.setFullYear(ret.getFullYear() + units); checkRollover();  break;
-    case 'quarter':  ret.setMonth(ret.getMonth() + 3*units); checkRollover();  break;
-    case 'month'  :  ret.setMonth(ret.getMonth() + units); checkRollover();  break;
-    case 'week'   :  ret.setDate(ret.getDate() + 7*units);  break;
-    case 'day'    :  ret.setDate(ret.getDate() + units);  break;
-    case 'hour'   :  ret.setTime(ret.getTime() + units*3600000);  break;
-    case 'minute' :  ret.setTime(ret.getTime() + units*60000);  break;
-    case 'second' :  ret.setTime(ret.getTime() + units*1000);  break;
-    default       :  ret = undefined;  break;
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+ 
+const dateAdd = (date, interval, units) => {
+  if (!(date instanceof Date)) return undefined
+ 
+  let ret = new Date(date) // don't change original date
+ 
+  const checkRollover = () => {
+    if (ret.getDate() !== date.getDate()) {
+      ret.setDate(0)
+    }
   }
-  return ret;
+ 
+  switch (String(interval).toLowerCase()) {
+    case 'year':
+        ret.setFullYear(ret.getFullYear() + units)
+        checkRollover()
+        break;
+    case 'quarter':
+        ret.setMonth(ret.getMonth() + 3 * units)
+        checkRollover()
+        break;
+    case 'month':
+        ret.setMonth(ret.getMonth() + units)
+        checkRollover()
+        break;
+    case 'week':
+        ret.setDate(ret.getDate() + 7 * units)
+        break;
+    case 'day':
+        ret.setDate(ret.getDate() + units)
+        break;
+    case 'hour':
+        ret.setTime(ret.getTime() + units * 3600000)
+        break;
+    case 'minute':
+        ret.setTime(ret.getTime() + units * 60000)
+        break;
+    case 'second':
+        ret.setTime(ret.getTime() + units * 1000)
+        break;
+    default:
+        ret = undefined
+        break;
+  }
+ 
+  return ret
 }
-
+ 
 client.on('message', msg => {
   if (msg.content === 'blacklotus') {
-    var taken = new Date();
-    var next = dateAdd(taken, 'minute', 45);
-    var takenHours = String(taken.getHours()).padStart(2, "0");
-    var takenMinutes = String(taken.getMinutes()).padStart(2, "0");
-    var nextHours = String(next.getHours()).padStart(2, "0");
-    var nextMinutes = String(next.getMinutes()).padStart(2, "0");
-    
-    var output = `TAKEN: ${ takenHours }:${ takenMinutes }\nNEXT: ${ nextHours }:${ nextMinutes }`;
-    
-    msg.channel.send(output);
+    let taken = new Date()
+    const next = dateAdd(taken, 'minute', 45)
+    const takenHours = String(taken.getHours()).padStart(2, '0')
+    const takenMinutes = String(taken.getMinutes()).padStart(2, '0')
+    const nextHours = String(next.getHours()).padStart(2, '0')
+    const nextMinutes = String(next.getMinutes()).padStart(2, '0')
+   
+    const output = `TAKEN: ${takenHours }:${takenMinutes}\nNEXT: ${nextHours}:${nextMinutes}`
+   
+    msg.channel.send(output)
   }
-});
-
-client.login(process.env.token);
+})
+ 
+client.login(process.env.token)
