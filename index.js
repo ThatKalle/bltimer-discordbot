dc2026c41fe1c3c7b87e5d783d9f9052b08eb246
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2020-05-20 22:37:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ client.on('message', msg => {
     }
 
     if (regex.test(command)) {
-      const now = dayjs(command, 'HH:mm').format('HH:mm')
-      const next = dayjs(now).add(45, 'minute').format('HH:mm')
+      const nowobj = dayjs(command, 'HH:mm')
+      const now = dayjs(nowobj).format('HH:mm')
+      const next = dayjs(nowobj).add(45, 'minute').format('HH:mm')
     
       msg.channel.send(`TAKEN: ${now}\nNEXT: ${next}`)
     }
