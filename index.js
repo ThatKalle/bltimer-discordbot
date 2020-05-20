e581ab886c788ae8a130e1b3ab346e6f8e214113
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2020-05-20 21:54:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,38 +9,15 @@ const timestamp = false
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
- 
-const addTime = (date, minutes) => {
-  return new Date(date.getTime() + minutes * 60000)
-}
- 
+
 client.on('message', msg => {
-	console.log(msg)
-	console.log(msg.content)
-  if (msg.content.includes('!lotus')) {
-    //const command = msg.content.toLowerCase().replace('!lotus', '').split(' ')[0]
-    const args = msg.content.slice(1).split(/ +/);
-	  console.log(args)
-    const command = args.shift().toLowerCase();
-	  console.log(command)
-    const regex = new RegExp("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$")
-    
-    if (!command.length) {
-      const now = dayjs().format('HH:mm')
-      const next = dayjs().add(45, 'minute').format('HH:mm')
-        
-      const output = `TAKEN: ${now}\nNEXT: ${next}`
-    
-      msg.channel.send(output)
-    } else {
-      if (regex.test(command)) {
-        const timestamp = true
-      }
-    }
-    if (timestamp) {
-      const output = `timestamp: ${command}`
-      msg.channel.send(output)
-    }
+  if (msg.content === `!lotus`) {
+    const now = dayjs().format('HH:mm')
+    const then = dayjs().add(45, 'minute').format('HH:mm')
+
+    const output = `TAKEN: ${now}\nNEXT: ${then}`
+
+    msg.channel.send(output)
   }
 })
 
