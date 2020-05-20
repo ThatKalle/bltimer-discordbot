9ad4e2cf5b0a1983c32b9ff8b2d9f5001f4468c5
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2020-05-20 20:21:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,42 +7,16 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
  
-const dateAdd = (date, interval, units) => {
-  if (!(date instanceof Date)) return undefined
- 
-  let ret = new Date(date)
- 
-  const checkRollover = () => {
-    if (ret.getDate() !== date.getDate()) {
-      ret.setDate(0)
-    }
-  }
- 
-  switch (String(interval).toLowerCase()) {
-    case 'minute':
-        ret.setTime(ret.getTime() + units * 60000)
-        break;
-    case 'second':
-        ret.setTime(ret.getTime() + units * 1000)
-        break;
-    default:
-        ret = undefined
-        break;
-  }
- 
-  return ret
+const addTime = (date, minutes) => {
+  return new Date(date.getTime() + minutes * 60000)
 }
  
 client.on('message', msg => {
   if (msg.content === 'blacklotus') {
-    let taken = new Date()
-    const next = dateAdd(taken, 'minute', 45)
-    const takenHours = String(taken.getHours()).padStart(2, '0')
-    const takenMinutes = String(taken.getMinutes()).padStart(2, '0')
-    const nextHours = String(next.getHours()).padStart(2, '0')
-    const nextMinutes = String(next.getMinutes()).padStart(2, '0')
-   
-    const output = `TAKEN: ${takenHours}:${takenMinutes}\nNEXT: ${nextHours}:${nextMinutes}`
+    const now = new Date()
+    const next = addTime(now, 45)
+
+    const output = `Taken: ${now}, Next: ${next}`
    
     msg.channel.send(output)
   }
