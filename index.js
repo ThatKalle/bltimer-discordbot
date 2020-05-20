c754b2db7ef5133144173313325cf56ef7c4b361
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2020-05-20 18:46:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,38 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+function dateAdd(date, interval, units) {
+  if(!(date instanceof Date))
+    return undefined;
+  var ret = new Date(date); //don't change original date
+  var checkRollover = function() { if(ret.getDate() != date.getDate()) ret.setDate(0);};
+  switch(String(interval).toLowerCase()) {
+    case 'year'   :  ret.setFullYear(ret.getFullYear() + units); checkRollover();  break;
+    case 'quarter':  ret.setMonth(ret.getMonth() + 3*units); checkRollover();  break;
+    case 'month'  :  ret.setMonth(ret.getMonth() + units); checkRollover();  break;
+    case 'week'   :  ret.setDate(ret.getDate() + 7*units);  break;
+    case 'day'    :  ret.setDate(ret.getDate() + units);  break;
+    case 'hour'   :  ret.setTime(ret.getTime() + units*3600000);  break;
+    case 'minute' :  ret.setTime(ret.getTime() + units*60000);  break;
+    case 'second' :  ret.setTime(ret.getTime() + units*1000);  break;
+    default       :  ret = undefined;  break;
+  }
+  return ret;
+}
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === 'blacklotus') {
+    var now = new date()
+    var time = dateAdd(new Date(), 'minute', 45)
+    
+    var output = '
+    TAKEN: now.getHours():time.getMinutes()\n
+    NEXT: time.getHours():time.getMinutes()';
+    
+    msg.reply(output);
   }
 });
 
 client.login(process.env.token);
+
+msg(output);
